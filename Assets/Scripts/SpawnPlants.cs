8e57d4203226d1d8cc70c7c369f0c365af18bad8
Evelyn Cruickshank <evelyaccessible@gmail.com>
2026-09-16 11:49:43 +1200

    Plants spawning in grids

--- Assets/Scripts/SpawnPlants.cs
+++ Assets/Scripts/SpawnPlants.cs
@@ -5,20 +5,28 @@ using System.Collections.Generic;
 public class SpawnPlants : MonoBehaviour
 {
     public List<GameObject> SpawnedObjs;
-    public float circleCircumf = 10f;
     public float chanceOfSpawning = 1f;
+    public float distBetween = 1.66f;
+    public bool[,] plantsThere;
 
+    private void Start()
+    {
+        plantsThere = new bool[6, 6];
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Random.Range(0f, 100f) < chanceOfSpawning)
         {
-            GameObject plant = SpawnedObjs[Random.Range(0, SpawnedObjs.Count)];
-            GameObject plampt = Instantiate(plant);
-            plampt.name = plant.name;
-            Vector2 pos = Random.insideUnitCircle * circleCircumf;
-            plampt.transform.position = transform.position + new Vector3(pos.x, 0, pos.y);
+            Vector2Int pos = new Vector2Int(Random.Range(0, 6), Random.Range(0, 6)); // spawns in six by six area
+            if (plantsThere[pos.x, pos.y])
+                return;
+            plantsThere[pos.x, pos.y] = true;
+            GameObject plantPref = SpawnedObjs[pos.x];
+            GameObject plampt = Instantiate(plantPref);
+            plampt.name = plantPref.name;
+            plampt.transform.position = transform.position + new Vector3(pos.x * distBetween, 0, pos.y * distBetween);
         }
     }
 }
